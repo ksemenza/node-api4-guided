205b3dc0e39a476b9da4e30e47c54ad7abd22f65
unknown <ksemenza13@gmail.com>
2020-03-12 12:03:32 -0400

    updated server

--- api/server.js
+++ api/server.js
@@ -1,4 +1,6 @@
+require('dotenv').config
 const express = require('express');
+const cors = require('cores')
 const helmet = require('helmet');
 
 const apiRouter = require('./api-router.js');
@@ -7,6 +9,18 @@ const server = express();
 
 server.use(helmet());
 
+function logger(req, res, next) {
+    console.log(
+        `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get('host')}`
+    );
+    next();
+}
+
+
+server.use(logger);
 server.use('/api', apiRouter);
+server.get('/', (req, res) => {
+    res.send('Welcome to the Thunder Dome');
+});
 
 module.exports = server;
