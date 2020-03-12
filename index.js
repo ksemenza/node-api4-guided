108caafefac135713595837b11086b158f1f1ded
unknown <ksemenza13@gmail.com>
2020-03-12 12:24:23 -0400

    add env.PORT

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const server = require('./api/server.js');
 
-const port = 7000;
+const port = proces.env.PORT || 7000;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
