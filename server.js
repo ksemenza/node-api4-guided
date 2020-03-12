fb82e8926f255e658a240658f12aaf6763695d3b
unknown <ksemenza13@gmail.com>
2020-03-12 12:49:13 -0400

    start

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const helmet = require('helmet');
 
-const apiRouter = require('./api-router.js');
+const apiRouter = require('./api/api-router.js');
 
 const server = express();
 
