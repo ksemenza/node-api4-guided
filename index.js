5a508e7327cb31f77065ee2d24d48708da007575
unknown <ksemenza13@gmail.com>
2020-03-12 12:29:30 -0400

    typo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config
 const express = require('express');
 const server = require('./api/server.js');
 
-const port = proces.env.PORT || 7000;
+const port = process.env.PORT || 7000;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
